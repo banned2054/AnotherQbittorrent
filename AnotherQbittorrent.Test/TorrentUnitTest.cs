8bc500be9a6ca4a35502fc7b893a4e9a087fd6a5
banned2054 <banned2054@163.com>
2025-02-01 23:31:47 +0900

    添加上传torrent文件的单元测试

--- AnotherQbittorrent.Test/TorrentUnitTest.cs
+++ AnotherQbittorrent.Test/TorrentUnitTest.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.Json;
+using System.Text.Json;
 using AnotherQbittorrent.Models.Enums;
 
 namespace AnotherQbittorrent.Test;
@@ -31,5 +31,24 @@ public class TorrentUnitTest
     public void TestDeleteTorrent()
     {
         _client.Torrent.DeleteTorrent("b963306bd91ff97492079b5510e91a111757322f");
+    }
+
+    [Test]
+    public async Task TestAddTorrent()
+    {
+        var torrentPaths =
+            new List<string>
+            {
+                "D:\\Downloads\\[BYRBT].[Sakurato] Okinawa de Suki ni Natta Ko ga Hougen Sugite Tsura Sugiru [04][AVC-8bit 1080p AAC][CHS&JPN].mp4.torrent"
+            };
+        var tags = "tv,mikan";
+        await _client.Torrent.AddTorrentAsync(
+                                              filePaths : torrentPaths,
+                                              savePath : "/downloads/Media/Anime",
+                                              paused : true,
+                                              tags : tags,
+                                              rename : "[Anime]在冲绳喜欢上的女孩方言讲得太过令人困扰 E04",
+                                              ratioLimit : -1
+                                             );
     }
 }
